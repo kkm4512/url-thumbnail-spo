bbb37406f19194ece893c276d8112941bf9babb5
kkm4512 <nayoun440@naver.com>
2025-11-22 12:48:41 +0900

    update app.vue added Analytics

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,13 @@
 <template>
   <NuxtLayout>
     <NuxtPage />
+    <Analytics />   <!-- ✅ 이 한 줄이 있어야 페이지뷰가 기록됨 -->
   </NuxtLayout>
 </template>
 
-
+<script setup>
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
 
 <style>
 html, body, #__nuxt, #__layout {
